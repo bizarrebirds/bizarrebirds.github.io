514ef48969d6b1ce83ee5f1909ba8ac7da3fd5d5
David Chu <davidchuyaya@gmail.com>
2025-11-30 16:30:13 -0800

    Tweaks to classes page

--- app/classes/page.tsx
+++ app/classes/page.tsx
@@ -1,11 +1,12 @@
 import FolderPage from "../folderpage";
 import { Description, Subheading } from "../text";
+import Link from "next/link";
 
 export default function ClassesPage() {
     return (
         <FolderPage className="flex flex-col sm:flex-row">
             <div className="flex-1">
-                <Subheading className="text-5xl mt-0!">Class list</Subheading>
+                <Subheading className="text-5xl mt-0!">Class List</Subheading>
                 <Subheading>English</Subheading>
                 <ul>
                     <li><Description>K-8 English, Language Arts, and related courses</Description></li>
@@ -46,13 +47,13 @@ export default function ClassesPage() {
 
                 <Description className="text-xs">*For all AP classes, tutoring sessions cover course content as well as AP test preparation (including essays, MCQs, DBQs, etc.). If you are not planning to take the AP test, please let me know as soon as you make that decision so that we can focus your tutoring sessions appropriately.</Description>
             </div>
-            <div className="flex-1 mt-12 sm:mt-0 sm:mx-4 sm:px-4 sm:py-4 sm:bg-white">
-                <Description>I offer the following:</Description>
+            <div className="flex-1 mt-12 rounded-2xl sm:mt-0 sm:mx-4 sm:px-4 sm:py-4 sm:bg-white">
+                <Subheading className="mt-0!">What I Offer</Subheading>
                 <ul>
-                    <li><Description>Initial Info Call (free)</Description></li>
-                    <li><Description>Trial Class ($50/hr)</Description></li>
-                    <li><Description>1:1 Tutoring Sessions ($100/hr)</Description></li>
-                    <li><Description>Small Group Tutoring Sessions (case-by-case)</Description></li>
+                    <li><Description><Link href="#initial-info-call">Initial Info Call (free)</Link></Description></li>
+                    <li><Description><Link href="#trial-class">Trial Class ($50/hr)</Link></Description></li>
+                    <li><Description><Link href="#one-on-one-tutoring">1:1 Tutoring Sessions ($100/hr)</Link></Description></li>
+                    <li><Description><Link href="#small-group-tutoring">Small Group Tutoring Sessions (case-by-case)</Link></Description></li>
                 </ul>
 
                 <Description>Each class is one hour long and is dedicated to one subject from the list below. If your student is interested in receiving tutoring for multiple subjects, then each subject will be scheduled as a separate hour.</Description>
@@ -62,19 +63,22 @@ export default function ClassesPage() {
                 <Description>Please see below for the list of classes that I offer tutoring for, and more information about each type of session.</Description>
 
 
-                <Subheading>Initial Info Call</Subheading>
+                <Subheading id="initial-info-call">Initial Info Call</Subheading>
                 <Description>
                 As a prospective family, you likely have a few questions to ask me about what I offer and how my style of tutoring can bolster your child's success. Similarly, I always have some questions for families about the class(es) your student is taking and/or what their college application goals are, what their timeline is like, and how they learn best. To get our questions answered, I offer 30-minute complementary phone or Zoom calls with prospective families. You may set one up via the link below.
                 </Description>
 
                 <a href="https://stembrainstutor.youcanbook.me/">Book an Initial Info Call</a><br />
 
-                <Subheading>Trial Class</Subheading>
+                <Subheading id="trial-class">Trial Class</Subheading>
                 <Description>I offer one hour-long trial class per student to allow you to get a sense of my teaching style. All trial classes are one-on-one between the teacher and the student, and they take place on Zoom.
                 During the trial class, we will get to know each other, and I will work through relevant class material with you. If you have current work that you would like assistance with, I recommend providing it to me ahead of time via email and bringing it to the trial class.</Description>
 
-                <Subheading>1:1 Tutoring Sessions</Subheading>
+                <Subheading id="one-on-one-tutoring">1:1 Tutoring Sessions</Subheading>
                 <Description>All classes take place on Zoom. I book tutoring sessions in blocks of 10 to allow predictability in our calendars.</Description>
+
+                <Subheading id="small-group-tutoring">Small Group Tutoring Sessions</Subheading>
+                <Description>Siblings or friends who are taking the same class (i.e. AP English Language) may inquire about small group classes at a reduced per-person rate. Typically, I allow a maximum of 3 students in a small group session so as to ensure I am able to spend ample time with each one individually. In your initial info call, please let me know if you are interested in this option - I would be happy to discuss our options. All listed classes are available for small group tutoring sessions.</Description>
             </div>
         </FolderPage>
     );
--- app/text.tsx
+++ app/text.tsx
@@ -1,5 +1,5 @@
-export function Subheading({ children, className = "" }) {
-    return <h1 className={`text-3xl font-bold my-4 mt-8 ${className}`}>{children}</h1>;
+export function Subheading({ children, className = "", id = "" }) {
+    return <h1 id={id} className={`text-3xl font-bold my-4 mt-8 ${className}`}>{children}</h1>;
 }
 
 export function Description({ children, className = "" }) {
