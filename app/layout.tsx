318b35c73d0cfcf80894fce7caf6ba2f0a54b5e8
David Chu <davidchuyaya@gmail.com>
2025-11-30 18:46:16 -0800

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,40 +3,41 @@ import { Cormorant_Garamond, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./header";
 import BookClassButton from "./bookclassbutton";
-import { Fragment } from "react/jsx-runtime";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const headerFont = Cormorant_Garamond({
-  variable: "--font-header",
-  subsets: ["latin"],
+    variable: "--font-header",
+    subsets: ["latin"],
 });
 
 const bodyFont = Inter({
-  variable: "--font-body",
-  subsets: ["latin"],
+    variable: "--font-body",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Humanities for STEM Brains",
-  description: "",
-  icons: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📝</text></svg>",
+    title: "Humanities for STEM Brains",
+    description: "",
+    icons: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📝</text></svg>",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${headerFont.variable} ${bodyFont.variable} antialiased w-full px-4 py-4`}>
-        <div className="max-w-7xl m-auto flex flex-col items-stretch">
-          <Header />
-          <div className="selected rounded-2xl">
-            {children}
-          </div>
-          <BookClassButton />
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <GoogleAnalytics gaId="G-HQFZGPCBNV" />
+            <body className={`${headerFont.variable} ${bodyFont.variable} antialiased w-full px-4 py-4`}>
+                <div className="max-w-7xl m-auto flex flex-col items-stretch">
+                    <Header />
+                    <div className="selected rounded-2xl">
+                        {children}
+                    </div>
+                    <BookClassButton />
+                </div>
+            </body>
+        </html>
+    );
 }
