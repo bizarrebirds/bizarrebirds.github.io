a75b485c7a8682a2c2c25d53215d88c3e9cfd727
David Chu <davidchuyaya@gmail.com>
2026-01-17 22:25:51 -0800

    Add tiktok

--- app/folderpage.tsx
+++ app/folderpage.tsx
@@ -1,7 +1,15 @@
+import ButtonBase from "@mui/material/ButtonBase";
+import Image from "next/image";
+
 export default function FolderPage({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
         <div className={`w-full min-h-screen px-10 py-10 ${className}`}>
             {children}
+            <div className="flex flex-col items-center">
+                <ButtonBase href="https://www.tiktok.com/@your.humanities.t" target="_blank">
+                    <Image src="TikTok-logo-RGB-Horizontal-black.png" alt="TikTok" width={10001} height={4168} className="w-50 self-center" />
+                </ButtonBase>
+            </div>
         </div>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,6 @@ export default function RootLayout({
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
                 <title>{title}</title>
                 <script
                     type="application/ld+json"
