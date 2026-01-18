ba23e345830c912c5316d820f42700e3018daccf
David Chu <davidchuyaya@gmail.com>
2026-01-17 22:08:30 -0800

    Add Json ld for seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,21 @@ const bodyFont = Inter({
     subsets: ["latin"],
 });
 
+const title = "Humanities for STEM Brains - 1:1 Tutoring";
+const description = "Uncover the hidden structure of the humanities with my logic-based systematic method for essay writing, grammar, and text analysis.";
+
 export const metadata: Metadata = {
-    title: "Humanities for STEM Brains - 1:1 Tutoring",
-    description: "Uncover the hidden structure of the humanities with my logic-based systematic method for essay writing, grammar, and text analysis.",
+    title,
+    description,
 };
+
+const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Product',
+    name: title,
+    image: '/favicon.ico',
+    description,
+}
 
 export default function RootLayout({
     children,
@@ -30,10 +41,17 @@ export default function RootLayout({
             <GoogleAnalytics gaId="G-HQFZGPCBNV" />
             <head>
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" href="/favicon.ico" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
-                <title>Humanities for STEM Brains - 1:1 Tutoring</title>
+                <title>{title}</title>
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify(jsonLd).replace(/</g, '\\u003c'),
+                    }}
+                />
             </head>
             <body className={`${headerFont.variable} ${bodyFont.variable} antialiased w-full px-4 py-4`}>
                 <div className="max-w-7xl m-auto flex flex-col items-stretch">
